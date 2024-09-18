9b091587d0aa1d250104a1ca173b57a78dd794ed
Nithin-Alagar-ICD2O1-01 <nithin.alagar@student.tdsb.on.ca>
2024-09-18 14:16:13 +0000

    nithin

--- PP1_3.py
+++ PP1_3.py
@@ -1,15 +1,25 @@
-
+'''
+Lesson 3 - Using Strings
+Author: Nithin Alagar
+Date Created: September 17th, 2024
+Date Last Modified: September 17th, 2024
+'''
 
 def q1():
   #Write Assignment code here
   #Remember your code must be indented once
-
+  #a)
+  print(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * * \n * * * * * *\n* * * * * *")
+  #b)
+  print("* * * * *\n*       *\n*       *\n*       *\n* * * * *")
+  #c)
+  print("* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *")
 def q2():
   #Write Assignment code here
-  
+  print("hello ") #print("world")
 def q3():
   #Write Assignment code here
-  
+  print("Man\nBoard\nMan Overboard")
 
 
 #Do not alter anything after this line
